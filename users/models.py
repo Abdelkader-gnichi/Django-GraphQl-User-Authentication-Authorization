22ffd6ec811f264b70101532e5d33c1916f3b4f6
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-28 16:15:46 +0100

    rm required attribute; perform migrations

--- users/models.py
+++ users/models.py
@@ -15,7 +15,7 @@ class BaseModelMixin(models.Model):
 
 class CustomUser(AbstractUser, BaseModelMixin):
     
-    email = models.EmailField(max_length=255, blank=False, null=False, unique=True, required=True, verbose_name='email')
+    email = models.EmailField(max_length=255, blank=False, null=False, unique=True, verbose_name='email')
 
     USERNAME_FIELD = 'username' # default value (username): unique identifier for authentication (login)  mechanism
     EMAIL_FIELD = 'email' # default value (email): Specifies the name of the field that holds the user's email address for communication and identification (like password resets).
