c9c1150926c469339afbae8fb47747754492c3f1
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-28 16:04:36 +0100

    create a custom user model; install django-graphql-jwt

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+
+class BaseModelMixin(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+
+class CustomUser(AbstractUser, BaseModelMixin):
+    
+    email = models.EmailField(max_length=255, blank=False, null=False, unique=True, required=True, verbose_name='email')
+
+    USERNAME_FIELD = 'username' # default value (username): unique identifier for authentication (login)  mechanism
+    EMAIL_FIELD = 'email' # default value (email): Specifies the name of the field that holds the user's email address for communication and identification (like password resets).
+
+
+
